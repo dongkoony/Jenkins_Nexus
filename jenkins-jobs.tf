7c2c9ccd1a3c26d6c3b5f7d24857e4402b244c04
donghyeonshin <donghyun4591@gmail.com>
2024-06-05 13:25:03 +0900

    -

--- jenkins-jobs.tf
+++ jenkins-jobs.tf
@@ -6,18 +6,18 @@ data "template_file" "job_xml" {
   }
 }
 
-resource "null_resource" "wait_for_instance" {
-  provisioner "local-exec" {
-    command = <<EOT
-for i in {1..10}; do
-  ssh -o StrictHostKeyChecking=no -i /home/ubuntu/Project/donghyeon.pem ubuntu@${module.jenkins_master.public_ip} 'echo Instance is ready' && break || sleep 30;
-done
-EOT
-  }
-}
+# resource "null_resource" "wait_for_instance" {
+#   provisioner "local-exec" {
+#     command = <<EOT
+# for i in {1..10}; do
+#   ssh -o StrictHostKeyChecking=no -i /home/ubuntu/Project/donghyeon.pem ubuntu@${module.jenkins_master.public_ip} 'echo Instance is ready' && break || sleep 30;
+# done
+# EOT
+#   }
+# }
 
 resource "null_resource" "jenkins_jobs" {
-  depends_on = [module.jenkins_master, module.jenkins_worker, null_resource.wait_for_instance]
+  depends_on = [module.jenkins_master, module.jenkins_worker]
 
   provisioner "local-exec" {
     command = <<EOT
